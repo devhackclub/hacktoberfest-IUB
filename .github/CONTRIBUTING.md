116fa60cadc993b75d599cb1127b069248aaf26e
mfaeezshabbir <faizytech1st@gmail.com>
2023-10-17 12:14:23 +0500

    First Commit

---
+++ .github/CONTRIBUTING.md
@@ -0,0 +1,11 @@
+# Contributing Guidelines
+
+Thank you for considering contributing to our Student Contributions Repository. To contribute, please follow these guidelines:
+
+1. Fork this repository.
+2. Add your profile details to the `contributions.json` file.
+3. Create a pull request.
+
+Be respectful and considerate when sharing your information.
+
+Thank you for being a part of our community!
---
+++ script.js
@@ -0,0 +1,37 @@
+// Create an array to store contributions (dummy contribution)
+const contributions = [
+  {
+    name: "Faeez",
+    image: "dummy.jpg",
+    department: "Software Engineering",
+    favoriteTech: "TypeScript",
+  },
+];
+
+const contributionsContainer = document.getElementById("contributions");
+
+contributions.forEach((contribution) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  // Populate the card with contribution data
+  const image = document.createElement("img");
+  image.src = contribution.image;
+  image.alt = "Student Image";
+
+  const name = document.createElement("h2");
+  name.textContent = contribution.name;
+
+  const department = document.createElement("p");
+  department.textContent = contribution.department;
+
+  const favoriteTech = document.createElement("p");
+  favoriteTech.textContent = `Favorite Language: ${contribution.favoriteTech}`;
+
+  card.appendChild(image);
+  card.appendChild(name);
+  card.appendChild(department);
+  card.appendChild(favoriteTech);
+
+  contributionsContainer.appendChild(card);
+});
